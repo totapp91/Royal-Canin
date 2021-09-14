ceb30773aba5dd3b5582be51cc55775e68f4c462
Tóth Attila <attila.toth@athalie.hu>
2021-09-14 20:01:59 +0200

    Add image url generator to repo

---
+++ Royal Canin macska eledelek/ImageUrlGenerator.ps1
@@ -0,0 +1,32 @@
+cls
+
+$path = Read-Host "Add meg a mappát"
+
+
+$pictures = Get-ChildItem -Path $path | where name -NotLike *.docx | select FullName
+
+foreach($_ in $pictures) {
+
+$result = $_.FullName.Replace("C:\Users\attila.toth\Documents\Azure DevOps\Web development\", "https://github.com/totapp91/Royal-Canin/raw/main/")
+$result = $result.Replace("\", "/")
+$result = $result.Replace(" ", "%20")
+$result += "|"
+
+$ImageUrls += $result
+
+}
+
+
+
+$ImageUrls = $ImageUrls.Substring(0, $ImageUrls.Length-1)
+
+"Ezt másold be:
+
+"
+$ImageUrls
+
+Remove-Variable ImageUrls
+
+"
+
+"
