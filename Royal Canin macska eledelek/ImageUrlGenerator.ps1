53260459fde45c4c827bbb8acca7fde7e7bf28d2
Tóth Attila <attila.toth@athalie.hu>
2021-09-15 11:42:55 +0200

    Törölve a ImageUrls file generálás

--- Royal Canin macska eledelek/ImageUrlGenerator.ps1
+++ Royal Canin macska eledelek/ImageUrlGenerator.ps1
@@ -3,11 +3,13 @@ cls
 $path = Read-Host "Add meg a mappát"
 
 
-$pictures = Get-ChildItem -Path $path | where name -NotLike *.docx | select FullName
+$pictures = Get-ChildItem -Path $path | where name -NotLike *.docx | where name -NotLike *.txt | select FullName
+
+$pictures.Count
 
 foreach($_ in $pictures) {
 
-$result = $_.FullName.Replace("C:\Users\attila.toth\Documents\Azure DevOps\Web development\", "https://github.com/totapp91/Royal-Canin/raw/main/")
+$result = $_.FullName.Replace("C:\Users\attila.toth\Documents\Azure DevOps\Web development\Royal-Canin", "https://github.com/totapp91/Royal-Canin/raw/main")
 $result = $result.Replace("\", "/")
 $result = $result.Replace(" ", "%20")
 $result += "|"
@@ -20,7 +22,6 @@ $ImageUrls += $result
 
 $ImageUrls = $ImageUrls.Substring(0, $ImageUrls.Length-1)
 
-$ImageUrls | Out-File -FilePath $path\ImageUrls.txt
 
 "Ezt másold be:
 
